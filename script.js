0924392969a5a4c21132d08dc63a92cc04730012
mohammedkaif01 <kaifofficial67@gmail.com>
2024-07-22 16:31:10 +0530

    new change

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-for(var i=1; i<=5; i++){
-    console.log("kaif");
-}
+let nav = document.querySelector("#burger");
+let drop = document.querySelector(".dropdown");
+
+// function showsidebar (){
+
+//     drop.style.display="flex";
+//     console.log("clicked");
+// }
+let display = "flex";
+
+nav.addEventListener('click', () => {
+    // drop.style.display="flex";
+    if (display === "flex") {
+        display = "flex";
+        drop.style.display = "flex";
+
+    } else {
+        // display = "none";
+        drop.style.display = "none";
+    }
+    console.log("clicked");
+})
+
